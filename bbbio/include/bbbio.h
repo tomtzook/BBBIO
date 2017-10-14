99f0c2721f6a7e5f19d2ef988c4552ac15c5db27
Tom Tzook <tomtzook@gmail.com>
2017-10-14 20:22:37 +0300

    Added project template

---
+++ bbbio/include/bbbio.h
@@ -0,0 +1,13 @@
+/*
+ * bbbio.h
+ *
+ *  Created on: Oct 14, 2017
+ *      Author: root
+ */
+
+#ifndef BBBIO_BBBIO_H_
+#define BBBIO_BBBIO_H_
+
+
+
+#endif /* BBBIO_BBBIO_H_ */
---
+++ bbbio/src/bbbio.c
@@ -0,0 +1,8 @@
+/*
+ * bbbio.c
+ *
+ *  Created on: Oct 14, 2017
+ *      Author: root
+ */
+
+
